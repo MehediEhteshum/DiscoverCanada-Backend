8de0487570468e4ee6eb21da1da2eab77dc86639
Mehedi Ehteshum <me69oshan@gmail.com>
2021-05-21 16:11:50 -0600

    var name update;

--- controllers/provinces.js
+++ controllers/provinces.js
@@ -1,7 +1,7 @@
-const env = require("../db_connect");
+const db_connect = require("../db_connect");
 
 // connect to MySQL db by creating connection pool
-const pool = env.pool;
+const pool = db_connect.pool;
 
 let provincesGetter = (_, res) => {
     pool.getConnection((err, connection) => {
